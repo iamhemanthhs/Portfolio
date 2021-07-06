34a6e81ab00afcd4bbe99ec767b4d1bd17b19e68
Hemanth H S <iam.hemanthhs@gmail.com>
2021-07-06 21:38:26 +0530

    initial commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -30,6 +30,38 @@ export default [
       },
     ],
   },
+
+  
+  {
+    slug: "https://github.com/iamhemanthhs/Student-Database-Management-and-Certificate-Maker",
+    title: "Student Database and Certificate Maker",
+    description: "GUI based application to maintain student database and create certificates. It also sends a security notification via Telegram Bot using Telegram API whenever someone access the database.",
+    year: "2021",
+    links: [
+      {
+        label: "Github",
+        url: "https://github.com/iamhemanthhs/Student-Database-Management-and-Certificate-Maker",
+      },
+      
+    ],
+  },
+
+
+  {
+    slug: "https://github.com/iamhemanthhs/Payslip-Maker",
+    title: "Payslip Maker for an Organization",
+    description: "GUI based application using python which incorporates data from Excel Sheet along with the payslip template to create individual payslips in pdf format.",
+    year: "2021",
+    links: [
+      {
+        label: "Github",
+        url: "https://github.com/iamhemanthhs/Payslip-Maker",
+      },
+      
+    ],
+  },
+
+
   {
     slug: "https://hemanthblogx.herokuapp.com",
     title: "Blog Site",
@@ -47,6 +79,37 @@ export default [
     ],
   },
 
+
+  {
+    slug: "https://github.com/iamhemanthhs/Send-To-Telegram",
+    title: "Send To Telegram",
+    description: "A windows application used to send the important messages to Telegram account using Telegram bot.",
+    year: "2020",
+    links: [
+      {
+        label: "Github",
+        url: "https://github.com/iamhemanthhs/Send-To-Telegram",
+      },
+      
+    ],
+  },
+
+
+  {
+    slug: "https://github.com/iamhemanthhs/Whatsapp-Online-Monitor",
+    title: "Whatsapp Online Monitor",
+    description: "Monitor the online status of a contact, collect and analyze online stats during a day and sends a notification via SMS using python.",
+    year: "2020",
+    links: [
+      {
+        label: "Github",
+        url: "https://github.com/iamhemanthhs/Whatsapp-Online-Monitor",
+      },
+      
+    ],
+  },
+
+  
   {
     slug: "https://toodolistx.herokuapp.com",
     title: "To-Do List",
